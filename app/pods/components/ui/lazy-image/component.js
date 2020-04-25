8170c11f8d31b6b05ecab1bd6e7051e35c50f9cf
Greg Hogan <greg@Gregs-MacBook-Pro.local>
2020-04-25 20:32:02 +0100

    Update lazy image component
    - Simplify component logic for lazy image.
    - Port some of the component logic of lazy image to the template.
    - Add new cover option to remove need for img direct CSS in other components.

--- app/pods/components/ui/lazy-image/component.js
+++ app/pods/components/ui/lazy-image/component.js
@@ -1,21 +1,20 @@
 import Component from '@ember/component';
-import { get, observer, set } from '@ember/object';
-import { alias } from '@ember/object/computed';
+import { get, set } from '@ember/object';
 
 
 export default Component.extend({
   classNames: 'lazy-image',
+  classNameBindings: ['shouldImageCover:lazy-image--image-cover:lazy-image--image-default'],
   
-  srcSet: null,
-  sizes: null,
+  alt: '',
+  srcSet: '',
+  sizes: '',
   isCurrentlyVisible: false,
-
-  placeholderSrc: alias('srcSet.pl.src'),
+  shouldImageCover: false,
 
   hasLoaded: false,
   hasPlaceholder: false,
   hasErroredPlaceholder: false,
-  srcSetStringified: '',
   offsetSrc: '',
   offsetSrcSet: '',
   offsetSizes: '100w',
@@ -24,23 +23,23 @@ export default Component.extend({
   init() {
     this._super();
 
-    this._setPlaceholderError = () => this.setPlaceholderError();
-    this._setStateHasLoaded = () => this.setStateHasLoaded();
-
-    this.setOffsetSrc();
-    this.setStringifySrcSet();
-    this.setOffsetPlaceholderSrc();
+    this.createInstanceEventHandlers();
+    this.createSrcStrings();
   },
   
   didInsertElement() {
     this._super();
     
     this.bindEvents();
-    this.setStateHasPlaceholder();
   },
   
   willDestroyElement() {
     this.unbindEvents();
+  },
+
+  createInstanceEventHandlers() {
+    this._setPlaceholderError = () => this.setPlaceholderError();
+    this._setStateHasLoaded = () => this.setStateHasLoaded();
   },
   
   bindEvents() {
@@ -68,30 +67,28 @@ export default Component.extend({
       lazyImagePlaceholder.removeEventListener('error', this._setPlaceholderError);
     }
   },
-  
-  setOffsetSrc() {
-    const srcObject = get(this, 'srcSet') || {}; //where is this being set?
-    const srcMd = Object.entries(srcObject).find((arr) => arr[0] === 'md'); //? prop names?
-    
-    set(this, 'offsetSrc', (srcMd && srcMd[1] && srcMd[1].src || ''));
-  },
-  
-  setOffsetPlaceholderSrc() {
-    if (get(this, 'srcSet.pl.src')) { // if you already have it here, is there code above needed?
-    }
-      const srcObject = get(this, 'srcSet') || {};
-      const srcPl = Object.entries(srcObject).find((arr) => arr[0] === 'pl');
-      
-      set(this, 'offsetPlaceholderSrc', (srcPl && srcPl[1] && srcPl[1].src || ''));
+
+  createSrcStrings() {
+    const srcSet = get(this, 'srcSet') || {};
+    const srcMainObj = Object.fromEntries(Object.entries(srcSet).filter((src) => src[0] !== 'pl'));
+
+    const placeholderSrc = srcSet?.pl?.src || '';
+    const mainSrc = srcSet?.md?.src || srcMainObj[0]?.src || '';
+    const mainSrcSet = this.stringifySrc(srcMainObj);
+    const mainSizes = `${get(this, 'sizes')}, 100w`.replace(/^, /g, '');
+
+    set(this, 'placeholderSrc', placeholderSrc);
+    set(this, 'offsetSrc', mainSrc);
+    set(this, 'offsetSrcSet', mainSrcSet);
+    set(this, 'offsetSizes', mainSizes);
+
+    set(this, 'hasPlaceholder', !!placeholderSrc);
   },
 
-  setStringifySrcSet() {
-    const srcObject = get(this, 'srcSet') || {};
-    const srcString = Object.keys(srcObject).filter((keyName) => keyName !== 'pl').map((imageSrc) => (
-      Object.values(srcObject[imageSrc]).filter(Boolean).join(' ')
+  stringifySrc(srcObject = {}) {
+    return Object.values(srcObject).map((sizeObject) => (
+      Object.values(sizeObject).filter(Boolean).join(' ').trim()
     )).join(', ').trim();
-
-    set(this, 'srcSetStringified', srcString);
   },
 
   setStateHasLoaded() {
@@ -104,29 +101,4 @@ export default Component.extend({
     set(this, 'hasErroredPlaceholder', true);
     set(this, 'hasPlaceholder', false);
   },
-
-  setStateHasPlaceholder() { // missleading name?
-    if (get(this, 'isCurrentlyVisible')) {
-      this.setOffsetSrcSet();
-      set(this, 'sizes', `${get(this, 'sizes')}, 100w`.replace(/^, /g, ''));
-    }
-
-    if (get(this, 'srcSet.pl')) {
-      set(this, 'hasPlaceholder', true);
-    }
-  },
-
-  setOffsetSrcSet() {
-    if (get(this, 'isCurrentlyVisible')) { // do need to check here
-      set(this, 'offsetSrcSet', get(this, 'srcSetStringified')); //to simplify, can get rid of this var and do whatever setStringifySrcSet does in this func
-      set(this, 'offsetSizes', `${get(this, 'sizes')}, 100w`.replace(/^, /g, ''));
-    }
-  },
-
-  isCurrentlyVisibleObserver: observer('isCurrentlyVisible', function() {
-    debugger;
-    if (get(this, 'isCurrentlyVisible')) { // wouldn't you do it alredy in init?
-      this.setOffsetSrcSet();
-    }
-  }),
 });
