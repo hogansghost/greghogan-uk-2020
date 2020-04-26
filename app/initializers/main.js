5d9f195456c24ef405a2fdbe509d2fbef74f4bf2
Greg Hogan <greg@Gregs-MacBook-Pro.local>
2020-04-26 18:04:23 +0100

    Add app deprecation warning initializer file
    - Silence jQuery warning because of Liquid Fire.

---
+++ app/initializers/main.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import { registerDeprecationHandler } from '@ember/debug';
+
+export function initialize() {
+  registerDeprecationHandler((message, options, next) => {
+    if (message.indexOf('Using Ember.$() has been deprecated') >= 0) {
+      return;
+    } else {
+      next(message, options);
+    }
+  });
+}
+
+export default { initialize };
