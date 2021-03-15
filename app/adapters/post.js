9ce0b71d1f8729b3563c2abea6d20c4d76c50161
Greg Hogan <me@greghogan.uk>
2021-03-15 01:14:44 +0100

    Replace Liquid Fire with simple page transition
    - Remove liquid fire dependency.
    - Remove all references to Liquid fire in copy for projects, etc.
    - Remove all transitions and code references to things like liquid-outlet
      from the codebase.
    - Remove CSS from app.scss that handled liquid fire transitions, etc.
    - Remove test post adapter that was commited by mistake.
    - Add logic into the main router on transition and transition complete
      (routeWillChange and routeDidChange as well as scheduleLater('atterRender'...)
      that will determine if the user is leaving the home page for a project and store
      the scroll position, delay the initial transition and set the transitiong
      state so the transitionContainer can fade out and then retry the transition so
      we can handle fading the content back in as well as setting the scroll position
      to the top or the stored value, depending on target.

--- app/adapters/post.js
+++
@@ -1,9 +0,0 @@
-import JSONAPIAdapter from '@ember-data/adapter/json-api';
-
-export default class PostAdapter extends JSONAPIAdapter {
-  host = 'https://demo.ghost.io/ghost/api/v3/content';
-
-  buildURL(...args) {
-    return `${super.buildURL(...args)}/?key=22444f78447824223cefc48062&include=tags,authors`;
-  }
-}
--- app/router.js
+++ app/router.js
@@ -1,5 +1,6 @@
 import EmberRouter from '@ember/routing/router';
 import { inject as service } from '@ember/service';
+import { later, scheduleOnce } from '@ember/runloop';
 import { tracked } from '@glimmer/tracking';
 
 import config from './config/environment';
@@ -8,22 +9,33 @@ import { fireGoogleEvent } from './functions/googleTrackingEvents';
 
 export default class Router extends EmberRouter {
   @service cookies;
+  @service scrollPosition;
+  @service pageTransition;
 
   location = config.locationType;
   rootURL = config.rootURL;
 
   @tracked hasTransitioned = false;
+  @tracked holdTransition = true;
+  @tracked transitionTargetIsHome = false;
 
   constructor() {
     super(...arguments);
 
     this.on('routeWillChange', (transition) => {
-      this.acceptCookieOnPageChange(transition);
+      if (!transition.isAborted) {
+        this.acceptCookieOnPageChange(transition);
+        this.transitionRouteOut(transition);
+      }
     });
 
     this.on('routeDidChange', (transition) => {
-      this.captureFirstTransitionOntoSite();
-      this.logGoogleAnalyticsOnPageChange(transition);
+      if (!transition.isAborted) {
+        this.captureFirstTransitionOntoSite();
+        this.logGoogleAnalyticsOnPageChange(transition);
+
+        this.transitionRouteIn(transition);
+      }
     });
   }
 
@@ -40,6 +52,55 @@ export default class Router extends EmberRouter {
   acceptCookieOnPageChange() {
     if (this.hasTransitioned) {
       this.cookies.acceptAllCookies();
+    }
+  }
+
+  isTransitionTargetHome(transition = {}) {
+    return transition?.to?.name === 'home' && transition?.to.name !== transition?.from?.name;
+  }
+
+  updateScrollPosition(transition) {
+    if (!transition.isAborted && !this.isTransitionTargetHome(transition)) {
+      this.scrollPosition.setScrollY();
+    }
+  }
+
+  transitionRouteOut(transition) {
+    this.updateScrollPosition(transition);
+
+    if (
+      transition?.from?.name &&
+      transition?.to?.name !== transition?.from?.name &&
+      transition?.to?.name !== this.holdTransition?.to?.name
+    ) {
+      this.holdTransition = transition;
+      this.pageTransition.setTransitioningStart();
+
+      transition.abort();
+
+      const transitionDelay = new Promise((resolve) => {
+        return later(() => {
+          return resolve();
+        }, this.pageTransition.duration);
+      })
+
+      return transitionDelay.then(() => {
+        this.holdTransition.retry().then(() => {
+          if (this.isTransitionTargetHome(this.holdTransition)) {
+            scheduleOnce('afterRender', this, this.scrollPosition.scrollToPreviousScrollPosition);
+          } else {
+            this.scrollPosition.scrollTo();
+          }
+
+          this.holdTransition = null;
+        });
+      });
+    }
+  }
+
+  transitionRouteIn(transition) {
+    if (!transition.isAborted && !transition.isActive) {
+      this.pageTransition.setTransitioningEnd();
     }
   }
 }
